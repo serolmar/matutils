2443bd65d2e52f9b0b310488ee0503f46f1a322f
SergioMarques <serolmar@hotmail.com>
2013-07-24 21:44:19 +0100

    Mudanca de directorias

---
+++ Mathematics/AlgebraicStructures/LogicInterfaces/IMatrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mathematics
+{
+    public interface IMatrix<CoordType, ObjectType>
+    {
+        ObjectType this[CoordType line, CoordType column] { get; set; }
+    }
+}
