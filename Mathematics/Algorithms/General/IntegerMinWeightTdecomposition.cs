278e632dd1cfe01f6faba4583b6fa509b8d3c551
SergioMarques <serolmar@hotmail.com>
2013-09-16 20:23:20 +0100

    Poucos avancos devido a falta de tempo

--- Mathematics/Algorithms/General/IntegerMinWeightTdecomposition.cs
+++ Mathematics/Algorithms/General/IntegerMinWeightTdecomposition.cs
@@ -12,7 +12,7 @@
     /// </summary>
     /// <typeparam name="CostType">O tipo de dados associado ao custo.</typeparam>
     public class IntegerMinWeightTdecomposition<CostType>
-        : IAlgorithm<int, int, SparseDictionaryMatrix<CostType>, int[]>
+        : IAlgorithm<int, SparseDictionaryMatrix<CostType>, int[]>
     {
         /// <summary>
         /// O anel responsável pelas operações.
@@ -46,23 +46,17 @@
         /// de custos especificada.
         /// </summary>
         /// <param name="n">O número a ser decomposto.</param>
-        /// <param name="p">O número de elementos na decomposição.</param>
         /// <param name="matrix">a matriz dos custos.</param>
         /// <returns>A decomposição caso exista e nulo caso contrário.</returns>
         public int[] Run(
-            int n, 
-            int p, 
+            int n,  
             SparseDictionaryMatrix<CostType> matrix)
         {
             if (matrix == null)
             {
                 throw new ArgumentNullException("matrix");
             }
-            else if (matrix.GetLength(1) != p)
-            {
-                throw new ArgumentException("The number of columns in cost matrix must match the value of p.");
-            }
-            else if (n < p)
+            else if (n < matrix.GetLength(0))
             {
                 // Para existir solução, o número a ser decomposto terá de ser superior ao número de elementos
                 // na decomposição.
@@ -70,7 +64,73 @@
             }
             else
             {
-                throw new NotImplementedException();
+                var p = matrix.GetLength(0);
+                var columns = matrix.GetLength(1);
+                var currentComponentAddition = 0;
+                var linesEnumerator = matrix.GetLines().GetEnumerator();
+                var state = linesEnumerator.MoveNext();
+                var verticesInfo = new VertexCostPair[n];
+                while (state)
+                {
+                    var currentLine = linesEnumerator.Current.Value;
+                    var columnsEnumerator = currentLine.GetColumns().GetEnumerator();
+                    var innerState = columnsEnumerator.MoveNext();
+                    while (innerState)
+                    {
+                        if (columnsEnumerator.Current.Key < n - p + 1 - currentComponentAddition)
+                        {
+
+                        }
+                        else
+                        {
+                            innerState = false;
+                        }
+                    }
+
+                    ++currentComponentAddition;
+                }
+
+                if (verticesInfo[n - 1] == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    var result = new int[p];
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private class VertexCostPair
+        {
+            private int vertex;
+
+            private CostType cost;
+
+            public int Vertex
+            {
+                get
+                {
+                    return this.vertex;
+                }
+                set
+                {
+                    this.vertex = value;
+                }
+            }
+
+            public CostType Cost
+            {
+                get
+                {
+                    return this.cost;
+                }
+                set
+                {
+                    this.cost = value;
+                }
             }
         }
     }
