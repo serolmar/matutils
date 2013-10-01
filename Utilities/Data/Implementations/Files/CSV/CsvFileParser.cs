c042a12da2318ad969dabc85d6e45f8f77062afd
SergioMarques <serolmar@hotmail.com>
2013-10-01 21:34:37 +0100

    Continuacao da leitura do csv

---
+++ Utilities/Data/Implementations/Files/CSV/CsvFileParser.cs
@@ -0,0 +1,149 @@
+﻿namespace Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Utilities.Parsers;
+
+    internal class CsvFileParser
+    {
+        /// <summary>
+        /// O separador de linha.
+        /// </summary>
+        private string lineSeparator;
+
+        /// <summary>
+        /// O separador de coluna.
+        /// </summary>
+        private string columnSeparator;
+
+        /// <summary>
+        /// Os delimitadores.
+        /// </summary>
+        private Dictionary<string, string> delimiters;
+
+        /// <summary>
+        /// Lista com todos os estados necessários para processar o csv.
+        /// </summary>
+        private List<IState<CharSymbolReader<string>, string, string>> states;
+
+        /// <summary>
+        /// Referência para a tabela a ser carregada.
+        /// </summary>
+        private ITabularItem currentTable;
+
+        /// <summary>
+        /// A linha corrente.
+        /// </summary>
+        private List<object> currentRow = new List<object>();
+
+        /// <summary>
+        /// O elemento corrente.
+        /// </summary>
+        private string currentElement = string.Empty;
+
+        /// <summary>
+        /// A linha actual.
+        /// </summary>
+        private int currentRowNumber = 0;
+
+        /// <summary>
+        /// A coluna actual.
+        /// </summary>
+        private int currentColumnNumber = 0;
+
+        /// <summary>
+        /// O valor actual lido.
+        /// </summary>
+        private string currentReadedValue = string.Empty;
+
+        public CsvFileParser(
+            string lineSeparator,
+            string columnSeparator,
+            Dictionary<string, string> delimiters)
+        {
+            this.lineSeparator = lineSeparator;
+            this.columnSeparator = columnSeparator;
+            this.delimiters = delimiters;
+            this.InitStates();
+        }
+
+        private ITabularItem Read(TextReader reader)
+        {
+            this.currentTable = new TabularListsItem();
+            this.currentRow.Clear();
+            this.currentElement = string.Empty;
+            var stringSymbolReader = new StringSymbolReader(reader, false);
+
+            StateMachine<CharSymbolReader<string>, string, string> machine = new StateMachine<CharSymbolReader<string>, string, string>(
+                this.states[0],
+                this.states[3]);
+            machine.RunMachine(stringSymbolReader);
+
+            return this.currentTable;
+        }
+
+        private void InitStates()
+        {
+            this.states = new List<IState<CharSymbolReader<string>, string, string>>();
+            this.states.Add(new DelegateStateImplementation<CharSymbolReader<string>, string, string>(0, "start", this.StartTransition));
+            this.states.Add(new DelegateStateImplementation<CharSymbolReader<string>, string, string>(1, "end", this.EndTransition));
+            this.states.Add(new DelegateStateImplementation<CharSymbolReader<string>, string, string>(2, "reading", this.ReadingTransition));
+            this.states.Add(new DelegateStateImplementation<CharSymbolReader<string>, string, string>(3, "delimiters", this.InsideDelimitersTransition));
+        }
+
+        private IState<CharSymbolReader<string>, string, string> StartTransition(SymbolReader<CharSymbolReader<string>, string, string> reader)
+        {
+            var symbol = reader.Get();
+            if (symbol.SymbolType == "eof")
+            {
+                return this.states[1];
+            }
+            if (symbol.SymbolType == this.lineSeparator)
+            {
+                this.currentTable.Add(this.currentRow);
+                return this.states[2];
+            }
+            else if (symbol.SymbolType == this.columnSeparator)
+            {
+                this.currentRow.Add(currentElement);
+                return this.states[2];
+            }
+            else if (this.delimiters.ContainsKey(symbol.SymbolType))
+            {
+                return this.states[3];
+            }
+            else
+            {
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private IState<CharSymbolReader<string>, string, string> EndTransition(SymbolReader<CharSymbolReader<string>, string, string> reader)
+        {
+            return null;
+        }
+
+        private IState<CharSymbolReader<string>, string, string> ReadingTransition(SymbolReader<CharSymbolReader<string>, string, string> reader)
+        {
+            var symbol = reader.Get();
+            throw new NotImplementedException(); 
+        }
+
+        private IState<CharSymbolReader<string>, string, string> InsideDelimitersTransition(SymbolReader<CharSymbolReader<string>, string, string> reader)
+        {
+            var symbolsStack = new Stack<string>();
+            var symbol = reader.Peek();
+            this.currentElement += symbol.SymbolValue;
+            while (symbolsStack.Count > 0)
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Utilities/Data/Implementations/Files/CSV/CsvFileReaderWriter.cs
@@ -0,0 +1,184 @@
+﻿namespace Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class CsvFileReaderWriter
+    {
+        /// <summary>
+        /// O separador de colunas no csv.
+        /// </summary>
+        private string columnSeparatorType;
+
+        /// <summary>
+        /// O separador de linhas no csv.
+        /// </summary>
+        private string lineSeparatorType;
+
+        /// <summary>
+        /// A lista com os delimitadores.
+        /// </summary>
+        private Dictionary<string, List<string>> delimiters = new Dictionary<string, List<string>>();
+
+        public CsvFileReaderWriter()
+        {
+            this.columnSeparatorType = "semi_colon";
+            this.lineSeparatorType = "new_line";
+        }
+
+        public CsvFileReaderWriter(string separatorType, string lineSeparatorType)
+        {
+            this.columnSeparatorType = separatorType;
+            this.lineSeparatorType = lineSeparatorType;
+        }
+
+        /// <summary>
+        /// Atribui e obtém o carácter que serve de separador do csv.
+        /// </summary>
+        public string SeparatorType
+        {
+            get
+            {
+                return this.columnSeparatorType;
+            }
+            set
+            {
+                this.columnSeparatorType = value;
+            }
+        }
+
+        public void MapDelimiters(string openDelimiter, string closeDelimiter)
+        {
+            if (string.IsNullOrWhiteSpace(openDelimiter))
+            {
+                throw new ArgumentException("An open delimiter must be provided.");
+            }
+            else if (string.IsNullOrWhiteSpace(closeDelimiter))
+            {
+                throw new ArgumentException("A close delimiter must be provided.");
+            }
+            else
+            {
+                var closedDelimiteres = default(List<string>);
+                if (this.delimiters.TryGetValue(openDelimiter, out closedDelimiteres))
+                {
+                    if (!closedDelimiteres.Contains(closeDelimiter))
+                    {
+                        closedDelimiteres.Add(closeDelimiter);
+                    }
+                }
+                else
+                {
+                    this.delimiters.Add(openDelimiter, new List<string>() { closeDelimiter });
+                }
+            }
+        }
+
+        public void UnmapDelimiters(string openDelimiter, string closeDelimiter)
+        {
+            if (!string.IsNullOrWhiteSpace(openDelimiter) &&
+                !string.IsNullOrWhiteSpace(closeDelimiter))
+            {
+                var closedDelimiteres = default(List<string>);
+                if (this.delimiters.TryGetValue(openDelimiter, out closedDelimiteres))
+                {
+                    closedDelimiteres.Remove(closeDelimiter);
+                    if (closedDelimiteres.Count == 0)
+                    {
+                        this.delimiters.Remove(openDelimiter);
+                    }
+                }
+            }
+        }
+
+        public void ClearDelimiters()
+        {
+            this.delimiters.Clear();
+        }
+
+        /// <summary>
+        /// Lê o csv dado o caminho do ficheiro.
+        /// </summary>
+        /// <param name="fileName">O caminho do ficheiro.</param>
+        /// <returns>O item tabular.</returns>
+        public ITabularItem Read(string fileName)
+        {
+            return this.Read(fileName, Encoding.ASCII);
+        }
+
+        public ITabularItem Read(string fileName, Encoding encoding)
+        {
+            var innerEncoding = encoding;
+            if (encoding == null)
+            {
+                innerEncoding = Encoding.ASCII;
+            }
+
+            var fileInfo = new FileInfo(fileName);
+            return this.Read(fileInfo, innerEncoding);
+        }
+
+        public ITabularItem Read(FileInfo fileInfo)
+        {
+            return this.Read(fileInfo, Encoding.ASCII);
+        }
+
+        public ITabularItem Read(FileInfo fileInfo, Encoding encoding)
+        {
+            if (fileInfo == null)
+            {
+                throw new ArgumentNullException("fileInfo");
+            }
+            else
+            {
+                var innerEncoding = encoding;
+                if (encoding == null)
+                {
+                    innerEncoding = Encoding.ASCII;
+                }
+
+                var stream = fileInfo.OpenRead();
+                return this.Read(stream, innerEncoding);
+            }
+        }
+
+        public ITabularItem Read(Stream stream)
+        {
+            return this.Read(stream, Encoding.ASCII);
+        }
+
+        public ITabularItem Read(Stream stream, Encoding encoding)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentException("stream");
+            }
+            else
+            {
+                var innerEncoding = encoding;
+                if (encoding == null)
+                {
+                    innerEncoding = Encoding.ASCII;
+                }
+
+                var textReader = new StreamReader(stream, innerEncoding);
+                return this.Read(textReader);
+            }
+        }
+
+        public ITabularItem Read(TextReader reader)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException("reader");
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
