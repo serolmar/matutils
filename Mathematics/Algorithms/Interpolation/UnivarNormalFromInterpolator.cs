ea41cc65e84b299b7a1abab5fb6704146becbe95
unknown <serolmar@hotmail.com>
2014-11-06 22:09:06 +0000

    Continuacao da implementacao do interpolador representado por um polinomio que se encontra na forma normal

--- Mathematics/Algorithms/Interpolation/UnivarNormalFromInterpolator.cs
+++ Mathematics/Algorithms/Interpolation/UnivarNormalFromInterpolator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Mathematics
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
@@ -8,6 +9,11 @@
     using Utilities;
     using Utilities.Collections;
 
+    /// <summary>
+    /// Permite construir o polinómio interpolador na forma normal.
+    /// </summary>
+    /// <typeparam name="SourceType">O tipo de objectos que constituem os coeficientes do polinómio.</typeparam>
+    /// <typeparam name="TargetType">O tipo de objectos que interpolam.</typeparam>
     public class UnivarNormalFromInterpolator<SourceType, TargetType>
         : ABaseInterpolator<SourceType, TargetType>
     {
@@ -15,6 +21,11 @@
         /// O polinómio interpolador actual.
         /// </summary>
         protected UnivariatePolynomialNormalForm<SourceType> interpolationgPolynomial;
+
+        /// <summary>
+        /// Indica se o polinómio é actual.
+        /// </summary>
+        protected bool updated;
 
         /// <summary>
         /// A matriz com os coeficientes simétricos nas coordenadas dos pontos.
@@ -27,19 +38,14 @@
         protected List<SourceType> inverseDifferencesVector;
 
         /// <summary>
+        /// Permite converter um valor inteiro num coeficiente do polinómio a ser interpolado.
+        /// </summary>
+        protected IConversion<SourceType, int> integerConversion;
+
+        /// <summary>
         /// O produto de todas as coordenadas dos pontos.
         /// </summary>
         protected SourceType productValue;
-
-        /// <summary>
-        /// O sinal actual.
-        /// </summary>
-        protected SourceType signal;
-
-        /// <summary>
-        /// A unidade aditiva inversa.
-        /// </summary>
-        protected SourceType additiveUnityInverse;
 
         /// <summary>
         /// Instancia uma nova instância de objectos do tipo <see cref="UnivarNormalFromInterpolator{SourceType, TargetType}"/>.
@@ -65,7 +71,14 @@
         {
             get
             {
-                throw new NotImplementedException();
+                if (this.updated)
+                {
+                    return this.interpolationgPolynomial;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
             }
         }
 
@@ -76,6 +89,14 @@
         /// <returns>A imagem que está associada ao objecto de acordo com a interpolação.</returns>
         public override TargetType Interpolate(SourceType sourceValue)
         {
+            if (this.updated)
+            {
+
+            }
+            else
+            {
+
+            }
             throw new NotImplementedException();
         }
 
@@ -94,15 +115,13 @@
                 this.inverseDifferencesVector = new List<SourceType>() { this.sourceField.AdditiveUnity };
                 var lastLine = new List<SourceType>() { this.sourceField.AdditiveUnity };
                 this.symmetricFuncMatrix.Add(lastLine);
-                this.productValue = this.pointsContainer[0].Item1;
-                this.signal = this.sourceField.AdditiveUnity;
+                this.productValue = this.sourceField.AdditiveInverse(this.pointsContainer[0].Item1);
 
                 for (int i = 1; i < this.pointsContainer.Count; ++i)
                 {
                     var currentPoint = this.pointsContainer[i].Item1;
+                    this.UpdateStateFromPointAddition(currentPoint);
                 }
-
-                throw new NotImplementedException();
             }
         }
 
@@ -131,7 +150,8 @@
             object sender,
             AddDeleteEventArgs<Tuple<SourceType, TargetType>> eventArgs)
         {
-            throw new NotImplementedException();
+            var firstCoord = eventArgs.AddedOrRemovedObject.Item1;
+            this.UpdateStateFromPointAddition(firstCoord);
         }
 
         /// <summary>
@@ -159,24 +179,214 @@
         }
 
         /// <summary>
+        /// Acutaliza o interpolador após a adição de um ponto.
+        /// </summary>
+        /// <param name="firstCoord">A primeira coordenada do ponto a ser adicionado.</param>
+        private void UpdateStateFromPointAddition(SourceType firstCoord)
+        {
+            var tasks = new Task[]{
+                new Task(()=>this.UpdateMatrixFromPointAddition(firstCoord)),
+                new Task(()=>this.UpdateVectorFromPointAddition(firstCoord))
+            };
+
+            Task.WaitAll(tasks);
+        }
+
+        /// <summary>
         /// Actualiza o estado do interpolador com o ponto dado.
         /// </summary>
         /// <param name="firstCoord">A primeira coordenada do ponto.</param>
-        private void UpdateStateFromPoint(SourceType firstCoord)
+        private void UpdateMatrixFromPointAddition(SourceType firstCoord)
         {
-            // A matriz pode ser actualizada de baixo para cima.
             var count = this.symmetricFuncMatrix.Count;
-            for (int i = count - 1; i > 0; --i)
+            if (count == 1)
             {
-                var currentLine = this.symmetricFuncMatrix[count];
+                var firstLine = this.symmetricFuncMatrix[0];
+                firstLine[0] = firstCoord;
+                firstLine.Add(this.productValue);
+                this.symmetricFuncMatrix.Add(new List<SourceType>(){
+                    this.sourceField.AdditiveInverse(this.sourceField.AdditiveUnity),
+                    this.sourceField.AdditiveUnity
+                });
 
-                // Actualiza todas as linhas com excepção da primeira.
-                Parallel.For(0, count, j =>
+                var value = this.sourceField.Add(
+                    firstCoord,
+                    this.sourceField.AdditiveInverse(this.pointsContainer[0].Item1));
+                value = this.sourceField.MultiplicativeInverse(value);
+                this.inverseDifferencesVector[0] = value;
+                this.inverseDifferencesVector.Add(value);
+
+                // Actualiza os parâmetros auxiliares.
+                value = this.sourceField.AdditiveInverse(firstCoord);
+                this.productValue = this.sourceField.Multiply(this.productValue, firstCoord);
+            }
+            else
+            {
+                // Insere a última linha
+                var vector = new SourceType[count];
+                var last = count - 1;
+                var lastLine = this.symmetricFuncMatrix[last];
+                Parallel.For(0, count, i =>
                 {
+                    vector[i] = this.sourceField.AdditiveInverse(lastLine[i]);
+                });
 
+                var insertionLine = new List<SourceType>(vector);
+                insertionLine.Add(this.sourceField.AdditiveUnity);
+                this.symmetricFuncMatrix.Add(insertionLine);
+
+                // Soma os valores da linha anterior e adiciona à lista actual.
+                vector[0] = this.productValue;
+                Parallel.For(1, count, i =>
+                {
+                    var accumulationLine = this.symmetricFuncMatrix[i - 1];
+                    var value = accumulationLine[0];
+                    var currentSignal = this.sourceField.AdditiveInverse(this.sourceField.AdditiveUnity);
+                    for (int j = 1; j < count; ++j)
+                    {
+                        var accumulationValue = accumulationLine[j];
+                        if (this.sourceField.IsAdditiveUnity(currentSignal))
+                        {
+                            value = this.sourceField.Add(value, accumulationValue);
+                            currentSignal = this.sourceField.AdditiveInverse(this.sourceField.AdditiveUnity);
+                        }
+                        else
+                        {
+                            value = this.sourceField.Add(
+                                value,
+                                this.sourceField.AdditiveInverse(accumulationValue));
+                            currentSignal = this.sourceField.AdditiveUnity;
+                        }
+                    }
+
+                    var conversionValue = this.integerConversion.DirectConversion(i);
+                    vector[i] = this.sourceField.Multiply(
+                        value,
+                        this.sourceField.MultiplicativeInverse(conversionValue));
+                });
+
+                // Adiciona os valores do vector às várias linhas da matriz.
+                Parallel.For(0, count, i =>
+                {
+                    var current = this.symmetricFuncMatrix[i];
+                    current.Add(vector[i]);
+                });
+
+                // A matriz pode ser actualizada de baixo para cima.
+                var previousLine = this.symmetricFuncMatrix[last];
+                --last;
+                for (int i = last; i > 0; --i)
+                {
+                    var currentLine = this.symmetricFuncMatrix[i];
+
+                    // Actualiza todas as linhas com excepção da primeira.
+                    Parallel.For(0, count, j =>
+                    {
+                        var temp = this.sourceField.Multiply(firstCoord, previousLine[j]);
+                        previousLine[j] = this.sourceField.Add(
+                            temp,
+                            this.sourceField.AdditiveInverse(currentLine[j]));
+                    });
+
+                    previousLine = currentLine;
+                }
+
+                previousLine = this.symmetricFuncMatrix[0];
+                Parallel.For(0, count, i =>
+                {
+                    previousLine[i] = this.sourceField.Multiply(previousLine[i], firstCoord);
                 });
             }
+        }
+
+        /// <summary>
+        /// Actualiza o vector independente a partir do ponto adicionado.
+        /// </summary>
+        /// <param name="firstCoord">A primeira coordenada do ponto adicionado.</param>
+        private void UpdateVectorFromPointAddition(SourceType firstCoord)
+        {
+            var count = this.inverseDifferencesVector.Count;
+            this.inverseDifferencesVector.Add(this.sourceField.AdditiveUnity);
+
+            var blockinCollection = new BlockingCollection<SourceType>();
+            var inserveProductConsumer = new InverseProductConsumer(
+                this.inverseDifferencesVector,
+                blockinCollection,
+                this.sourceField);
+            var consumerTask = new Task(() => inserveProductConsumer.Run());
+            Parallel.For(0, count, i =>
+            {
+                var value = this.sourceField.Add(
+                    firstCoord,
+                    this.sourceField.AdditiveInverse(this.pointsContainer[i].Item1));
+                value = this.sourceField.MultiplicativeInverse(value);
+                blockinCollection.Add(value);
+                this.inverseDifferencesVector[i] = this.sourceField.Multiply(
+                    this.inverseDifferencesVector[i],
+                    value);
+
+            });
+
+            blockinCollection.CompleteAdding();
+            consumerTask.Wait();
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Classe que representa um consumidor que permite actualizar a última entrada do vector.
+        /// </summary>
+        private class InverseProductConsumer
+        {
+            /// <summary>
+            /// O vector a ser actualizado.
+            /// </summary>
+            private List<SourceType> vector;
+
+            /// <summary>
+            /// O último índice do vector.
+            /// </summary>
+            private int lastIndex;
+
+            /// <summary>
+            /// A colecção que bloqueia.
+            /// </summary>
+            private BlockingCollection<SourceType> blockingCollection;
+
+            /// <summary>
+            /// O corpo responsável pelas operações sobre os coeficientes do polinómio interpolador.
+            /// </summary>
+            private IField<SourceType> sourceField;
+
+            /// <summary>
+            /// Instancia uma nova instância de objectos do tipo <see cref="InverseProductConsumer"/>.
+            /// </summary>
+            /// <param name="vector">O vector.</param>
+            /// <param name="blockingCollection">A colecção que bloqueia.</param>
+            /// <param name="sourceField">O corpo responsável pelas operações sobre os coeficientes.</param>
+            public InverseProductConsumer(
+                List<SourceType> vector,
+                BlockingCollection<SourceType> blockingCollection,
+                IField<SourceType> sourceField)
+            {
+                this.sourceField = sourceField;
+                this.vector = vector;
+                this.blockingCollection = blockingCollection;
+                this.lastIndex = this.vector.Count - 1;
+            }
+
+            /// <summary>
+            /// Executa o consumidor.
+            /// </summary>
+            public void Run()
+            {
+                while (!this.blockingCollection.IsCompleted)
+                {
+                    var value = this.blockingCollection.Take();
+                    this.vector[lastIndex] = this.sourceField.Multiply(
+                        vector[lastIndex],
+                        value);
+                }
+            }
         }
     }
 }
